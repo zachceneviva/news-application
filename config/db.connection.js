221b84e656f988522e4770f05064f02ebc49275c
zach-ceneviva <zceneviva@gmail.com>
2021-10-11 15:34:53 -0500

    Updated comments for MongoDB connection file

--- config/db.connection.js
+++ config/db.connection.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // Variables
 const connectionStr = process.env.MONGODB_URI;
 
+// Mongoose Connection Listeners
 mongoose.connection.on('connected', () => {
     console.log(`${new Date().toLocaleTimeString()} - MongoDB connected.`);
 });
@@ -16,4 +17,5 @@ mongoose.connection.on('disconnected', () => {
     console.log(`MongoDB has been disconnected.`);
 });
 
+// Mongoose connection to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/zashlios');
