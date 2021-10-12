71b75bed092177b3e4c6cf5a992d50cbacc3398b
zach-ceneviva <zceneviva@gmail.com>
2021-10-12 11:45:33 -0500

    Added post route for registering a new user

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -7,11 +7,27 @@ const router = express.Router();
 
 const {User} = require('../models');
 
+// GET route for Register page
 router.get('/register', (req, res) => {
     return res.render('./auth/register.ejs')
 });
 
+// GET route for Login page
 router.get('/login', (req, res) => {
     return res.render('./auth/login.ejs')
+});
+
+router.post('/register', async function (req, res) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hash;
+        const newUser = await User.create(req.body);
+
+        return res.redirect('/login');
+    } catch (error) {
+        console.log(error);
+        return res.send(error);
+    }
 });
 
