58a9291dddd3abb15fef556325e09bddaf1c24d8
ashleyromano <ashleye.romano@gmail.com>
2021-10-12 14:43:17 -0400

    added controllers and ejs pages

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -1,10 +1,36 @@
+
+//i think this deals with the 'comments' made by user
 const express = require('express');
-
 const router = express.Router();
+const { Article, User} = require("../models");
 
-// Home/Index Route
-router.get('/home', (req, res) => {
-    res.render("./news/home.ejs")
-})
+User.deleteMany({}, function (error, deletedUser) {
+  if (error) {
+    return console.log(error);
+  }
+  User.insertMany(
+    [
+      {
+        articleId: 0,
+        text: "Learned a lot!!",
+      },
+      {
+        articleId: 1,
+        text: "Hated it.",
+      },
+      {
+        articleId: 2,
+        text: "How can I learn more?",
+      },
+    ],
+    function (error, createdUsers) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("=== Seed Complete ===");
+      console.log(createdUsers);
+    }
+  );
+});
 
-module.exports = router
+module.exports = User;
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 require('../config/db.connection');
 
+
 module.exports = {
-    articles: require("./articles_controller"),
-}
+    articles: require("./photo_controllers"),
+    user: require("./user_controllers"),
+};
+  
+//can i change articles to article in file name and code for controllers? just copying patterns from past work 
+
