af6cf866b6ba92470fe6a85577b76b9550ed3141
ashleyromano <ashleye.romano@gmail.com>
2021-10-12 15:27:40 -0400

    ?

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -1,36 +1,92 @@
-
-//i think this deals with the 'comments' made by user
 const express = require('express');
 const router = express.Router();
-const { Article, User} = require("../models");
+const { Article, User } = require("../models");
 
-User.deleteMany({}, function (error, deletedUser) {
-  if (error) {
+
+//home / index
+router.get('/home', async function (req, res) {
+  try {
+    const articles = await Article.find({});
+  
+    const context = {
+      articles,
+    }
+    
+    res.render("./news/home.ejs", context);
+
+  } catch (error) {
     return console.log(error);
   }
-  User.insertMany(
-    [
-      {
-        articleId: 0,
-        text: "Learned a lot!!",
-      },
-      {
-        articleId: 1,
-        text: "Hated it.",
-      },
-      {
-        articleId: 2,
-        text: "How can I learn more?",
-      },
-    ],
-    function (error, createdUsers) {
-      if (error) {
-        return console.log(error);
-      }
-      console.log("=== Seed Complete ===");
-      console.log(createdUsers);
+});
+
+// Create new 
+router.get('/new', (req, res) => { 
+  res.render('new.ejs');
+});
+router.post('/', async (req, res) => {   //'/' maybe = '/home'?
+  try {
+    await Article.create( req.body )
+
+    return res.redirect('/articles');
+  } catch (error) {
+    return console.log(error);
+  }
+});
+
+//Show
+router.get("/:id", async (req, res, next) => {
+  try {
+    const article = await Article.findById(req.params.id);
+    const user = await User.find({ article: req.params.id }).populate('article'); 
+    const context = {
+      article,
+      review,
     }
+    return res.render("news/show.ejs", context);   //can we change news to articles?
+  } catch (error) {
+    console.log(error);
+    req.error = error;
+    return next();
+  }
+});
+
+//Edit
+router.get('/:articleId/edit', async (req, res) => {
+  try {  
+    const article = await Article.findById(req.params.articleId)
+    return res.render('edit.ejs', { article });
+  } catch (error) {
+    return console.log(error)
+  }
+});
+
+//Update
+router.put('/:articleId', (req, res) => {
+
+  Article.findByIdAndUpdate(
+      req.params.articleId,
+     {
+       $set: req.body
+     },
+      {
+        new: true
+      },
+      (error, updatedArticle) => {
+          if (error) return console.log(error);
+          
+          return res.redirect(`/news/${updatedArticle.id}`);
+      },
   );
 });
 
-module.exports = User;
+//Delete
+router.delete('/:articleId', (req, res) => {
+   Article.findByIdAndDelete( req.params.articleId, (error, deletedArticle) => {
+        if (error) return console.log(error);
+    
+        console.log(deletedArticle);
+        return res.redirect('/article');
+    });
+})
+
+module.exports = router
