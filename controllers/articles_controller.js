ece1efcefe1039df61e4bd38fc7b77e612c32f6f
zorkb <zachbuchs@gmail.com>
2021-10-12 13:53:53 -0700

    small changes after ashley's push

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -1,10 +1,17 @@
 const express = require('express');
-
+const { Article } = require('../models');
 const router = express.Router();
+
+//test data
+
 
 // Home/Index Route
 router.get('/home', (req, res) => {
-    res.render("./news/home.ejs")
-})
+    Article.find({}, (error, article) => {
+        res.render("./news/home.ejs", {article})
+    })
+});
+
+
 
 module.exports = router
